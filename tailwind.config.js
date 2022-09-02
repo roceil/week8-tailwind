89c6c0554d70720694ab56995014a85e699f2e28
roceil <a0978006326@gmail.com>
2022-09-02 22:28:33 +0800

    header&footer完成

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
 module.exports = {
   content: ["./app/**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '12px',
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '769px',
+      // => @media (min-width: 769px) { ... }
+
+      'lg': '991px',
+      // => @media (min-width: 991px) { ... }
+
+      'xl': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+    extend: {
+      colors: {
+        'primary': '#E6553B',
+        'secondary':'#F0F0F0',
+        'gray':'#808080',
+        'light_gray':'#00000029',
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
